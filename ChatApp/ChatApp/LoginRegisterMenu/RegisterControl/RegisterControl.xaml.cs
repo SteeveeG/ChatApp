4bebea68425b01996514d6bc26f8df1824e7928e
Steve g <119070310+SteeveeG@users.noreply.github.com>
2024-05-13 10:31:26 +0200

    small ui fix

--- ChatApp/ChatApp/LoginRegisterMenu/RegisterControl/RegisterControl.xaml.cs
+++ ChatApp/ChatApp/LoginRegisterMenu/RegisterControl/RegisterControl.xaml.cs
@@ -50,16 +50,17 @@ public partial class RegisterControl : UserControl
     }
 
 
-    private void OnSelectionChanged(object sender, RoutedEventArgs e)
-    {
-        SetFocus(Password.IsFocused, Name.IsFocused);
-    }
 
     private void SetFocus(bool passwordValue, bool nameValue)
     {
         ViewModel.PasswordFocused = passwordValue;
         ViewModel.NameFocused = nameValue;
     }
-    
-    
+
+
+    private void GotFocus(object sender, RoutedEventArgs e)
+    {
+        SetFocus(Password.IsFocused, Name.IsFocused);
+
+    }
 }
